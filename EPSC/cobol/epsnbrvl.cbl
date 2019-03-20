ef7aaa652c1994fb852835ab41a7aec8f72d9d79
Daniel Bruce <drbruce@us.ibm.com>
2019-03-20 14:21:15 -0400

    Indirect dependency test

--- EPSC/cobol/epsnbrvl.cbl
+++ EPSC/cobol/epsnbrvl.cbl
@@ -13,7 +13,7 @@
       *    OUTPUT:
       *       MONTHLY PAYMENT
       *
-      *    (C) 2018 IBM - Jim Hildner
+      *    (C) 2019 IBM - Jim Hildner
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SOURCE-COMPUTER. FLEX-ES.
